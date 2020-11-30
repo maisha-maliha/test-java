be1c96bd0e086eaa8f5f0e92881343ab382ce41a
maisha-maliha <officiallymaisha@gmail.com>
2020-11-30 14:19:52 +0600

    porblemn in name input second time

--- 1/student.java
+++ 1/student.java
@@ -8,18 +8,38 @@ class Student{
   String subject;
   void divide(){
     if(marks<=40){
-      System.out.printf("You failed");
+      System.out.println(name +"( " + age + ")"+",You failed. You got" + marks);
     } else if (marks>40 && marks <= 59){
-      System.out.printf("You got C grade");
+      System.out.println(name +"( " + age + ")"+",You got C grade. You got " + marks);
     } else if (marks>59 && marks <= 79){
-        System.out.printf("You got B grade");
+        System.out.println(name +"( " + age + ")"+",You got B grade. You got " + marks);
     } else {
-      System.out.printf("You got A grade");
+      System.out.println(name +"( " + age + ")"+",You got A grade. You got " + marks);
     }
   }
 }
 class Test{
   public static void main(String args[]){
     Scanner inp = new Scanner(System.in);
+    Student student = new Student();
+    Boolean value = true;
+    while(value){
+      System.out.println("Enter Name:");
+      student.name = inp.nextLine();
+      System.out.println("Enter Roll:");
+      student.roll = inp.nextInt();
+      System.out.println("Enter age:");
+      student.age = inp.nextInt();
+      System.out.println("Enter Marks:");
+      student.marks = inp.nextInt();
+      student.divide();
+      System.out.println("Do you want to contine: (y/n)");
+      char n = 'n';
+      char ans = inp.next().charAt(0);
+      if(ans == n){
+        value = false;
+      }
+    }
+    System.exit(0);
   }
 }
